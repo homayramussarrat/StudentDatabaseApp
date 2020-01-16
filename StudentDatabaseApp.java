fa2c1ed8b38b0233dd8cb5aafa55d01125e79167
homayramussarrat <51522781+homayramussarrat@users.noreply.github.com>
2020-01-15 23:19:41 -0500

    Add files via upload

---
+++ StudentDatabaseApp.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+		
+	
+		
+		
+		//Ask how many new students we want to add
+		
+		System.out.println("Enter number of new students to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		
+		Student[] students = new Student[numOfStudents]; //Array of objects
+		
+		
+		
+		//Create n number of new students 
+		
+		for(int i =0; i < numOfStudents; i++) {
+			
+			students[i] = new Student();
+			students[i].enroll();
+			students[i].payTuition();
+			
+		
+		}
+		
+        for(int i =0; i < numOfStudents; i++) {
+			
+			System.out.println(students[i].toString());
+			
+		
+		}
+		
+		
+		
+		
+
+	}
+
+}
